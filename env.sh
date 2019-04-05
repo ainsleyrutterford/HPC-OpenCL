fa16dc69bfdfb6dfd187cc7d0bd12304fff3e872
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-04-05 12:02:54 +0100

    Updated env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
 
+module load languages/intel-compiler-16-u2
+module load languages/gcc-7.1.0
 module load cuda/toolkit/7.5.18
 module load /newhome/pa13269/modules/modulefiles/llvm
